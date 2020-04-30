46d806159a65523596390facb9b83e833608402a
Edward Chou <triplec.light@gmail.com>
2020-04-30 14:52:16 +0800

    Update main code

--- CurrentMeter_V3.py
+++ CurrentMeter_V3.py
@@ -14,6 +14,7 @@ class CurrentOperate:
         self.currentTable = []
         self.tatalmAmS = 0
         self.totalmAH = 0
+        self.sampleEveryMicroSecond = 343
 
     def get_mA(self, adcValue):
         _voltage = round(adcValue * self.voltageScale, 6)
@@ -25,7 +26,7 @@ class CurrentOperate:
         self.currentTable = currentTable
         _tatalmAmS = 0
         # _mS = 1 / (343 / 1000)
-        _mS = (343/1000) / 1    # 每一個 sample time佔 1ms的多少
+        _mS = (self.sampleEveryMicroSecond/1000) / 1    # 每一個 sample time佔 1ms的多少
         maxCurrent = (self.refVoltage / self.sampleResistor) * 1000     # mA
         for _current in self.currentTable:
             data = _current.split(", ")
@@ -46,7 +47,7 @@ class CurrentOperate:
         passFirstData = True
         _date = self.currentTable[0].split(', ')[0]
         _time = self.currentTable[0].split(', ')[1]
-        f = open(filename, 'a+')
+        f = open(fileName, 'a+')
         f.write(self.currentTable[0] + "\n")
         for currentData in self.currentTable:
             currentData = currentData.split(", ")
@@ -58,9 +59,10 @@ class CurrentOperate:
                     writeData = '-, -, ' + currentData[2] + ', ' + currentData[3]
                     f.write(writeData + "\n")
 
-        writeData = _date + ", " + _time + ", 0 mS, 0 mA, " + str(self.tatalmAmS) + ' mAmS, ' + str(self.totalmAH) + ' mAH'
+        writeData = _date + ", " + _time + ", 0, 0, " + str(self.tatalmAmS) + ', ' + str(self.totalmAH)
         f.write(writeData + "\n")
         f.close()
+        print("> Write to File")
 
 
 class TimeCtrl:
@@ -99,28 +101,9 @@ ser.isOpen()
 adcValue = 0
 currentLevel = 2  # mA
 
-# timeTag = 0
-# dataCount = 0
-# count = 0
-# voltageSum = 0
-# sampleCount = 1
-# showCurrent = 0
-# voltage = 0
 tStart = time.time()
 currentTable = []
-# lastlocalTime = ""
-# newDataFlag = True
-# zeroCount = 0
-# timeShow = False
-# oldTimeTag = 0
-# tStart0 = time.time()
-# pEND1 = time.time()
-# continueTime = 0
-# state = 0
-# exeOnlyOneTime = True
 
-print('===========================================')
-print('===========================================')
 print('===========================================')
 current = CurrentOperate()
 timeCtrl = TimeCtrl()
@@ -128,7 +111,9 @@ ser.flushInput()
 filename = "CurrentV4_" + time.strftime("%Y%m%d%H%M", time.localtime()) + ".txt"
 # filename = "CurrentV4.txt"
 f = open(filename, 'w')
+f.write('Date, Time, Tag, mA, mA/mS, mAH' + "\n")
 f.close()
+print('> Create File:', filename)
 
 try:
     while True:
@@ -202,77 +187,15 @@ try:
                 newCurrent = current.get_mA(adcValue)
                 localTime = timeCtrl.getTime()
                 if newCurrent > currentLevel:
-                    # print(newCurrent)
                     tStart = time.time()
-
-                    #
-                    # if newDataFlag == True:
-                    #     localTime = time.strftime("%Y/%m/%d, %H:%M:%S", time.localtime())
-                    #     lastTime = localTime.split(", ")
-                    #     lastTime = lastTime[1]
-                    #     newDataFlag = False
-                    # else:
-                    #     localTime = "-, -"
-                    #
-                    # if timeShow == True:
-                    #     localTime = time.strftime("%Y/%m/%d, %H:%M:%S", time.localtime())
-                    #     lastTime = localTime.split(", ")
-                    #     lastTime = lastTime[1]
-                    #     timeShow = False
-                    # else:
-                    #     if (float(timeTag) - oldTimeTag) >= 1000:
-                    #         timeShow = True
-                    #         oldTimeTag = float(timeTag)
-                    #
                     timeCtrl.updateTag()
-                    showData = localTime + ", " + str(timeCtrl.tag) + " mS, " + str(newCurrent) + " mA"
+                    showData = localTime + ", " + str(timeCtrl.tag) + ", " + str(newCurrent) + ""
                     currentTable.append(showData)
-                    #
-                    # # print(showData)
-                    #
-
-                    # if dataCount > 291550:
-                    #     tatalmAmS = 0
-                    #     f = open(filename, 'a+')
-                    #     for i in currentTable:
-                    #         data = i.split(", ")
-                    #         current = float(data[3].replace(" mA", ""))
-                    #         if current < 330:
-                    #             mAmS = current / (1 / (sampleEveryMicroSecond / 1000))
-                    #             tatalmAmS += mAmS
-                    #             f.write(i + "\n")
-                    #
-                    #     tatalmAmS = round(tatalmAmS, 6)
-                    #     totalmAH = (tatalmAmS / 1000) / 3600
-                    #     totalmAH = format(totalmAH, '.10f')
-                    #
-                    #     f.write(data[0] + ", " + lastTime + ", " + timeTag + " mS, 0 mA, " + str(
-                    #         tatalmAmS) + " mA/mS, " + str(totalmAH) + " mAH")
-                    #     f.write("\n")
-                    #
-                    #     f.close()
-                    #     currentTable.clear()
-                    #     newDataFlag = True
-                    #     ser.flushInput()
-                    #
-                    #     print("Time= " + time.strftime("%Y/%m/%d, %H:%M:%S", time.localtime()))
-                    #     print("dataCount= " + str(round(((dataCount * sampleEveryMicroSecond) / 1000), 6)) + "ms")
-                    #     dataCount = 0
-                    #     print(str(tatalmAmS) + " mA/mS")
-                    #     print(str(totalmAH) + " mAH")
-                    #     print("Write to File")
-                    #     print("")
-                    #
-                    # zeroCount = 0
                 else:
                     tEnd = time.time()
-                #     tatalmAmS = 0
-                #     zeroCount += 1
-                #     # if (tEnd - tStart) > timeOut or zeroCount > int((timeOut * 1000) / (sampleEveryMicroSecond / 1000)):
                     if (tEnd - tStart) > timeCtrl.timeOut and len(currentTable) > 0:
                         tStart = time.time()
-                #         zeroCount = 0
-                #         print(currentTable)
+                        print("")
                         print("Time= " + localTime)
                         print("Data Length= " + str(timeCtrl.tag) + "ms")
                         timeCtrl.clrTag()
@@ -280,46 +203,8 @@ try:
                         totalmAH = current.get_TotalmAH(totalmAmS)
                         print(str(totalmAmS) + " mA/mS")
                         print(str(totalmAH) + " mAH")
-                        print("Write to File")
-                        print("")
                         current.writeToFile(filename)
                         currentTable.clear()
-
-                #             f = open(filename, 'a+')
-                #             for i in currentTable:
-                #                 data = i.split(", ")
-                #                 current = float(data[3].replace(" mA", ""))
-                #                 if current < 330:
-                #                     mAmS = current / (1 / (sampleEveryMicroSecond / 1000))
-                #                     tatalmAmS += mAmS
-                #                     f.write(i + "\n")
-                #
-                #             tatalmAmS = round(tatalmAmS, 6)
-                #             totalmAH = (tatalmAmS / 1000) / 3600
-                #             totalmAH = format(totalmAH, '.10f')
-                #
-                #             if timeTag!='0.0':
-                #                  f.write(data[0] + ", " + lastTime + ", " + timeTag + " mS, 0 mA, " + str(
-                #                      tatalmAmS) + " mA/mS, " + str(totalmAH) + " mAH")
-                #                  f.write("\n")
-                #
-                #
-                #                  currentTable.clear()
-                #                  newDataFlag = True
-                #                  ser.flushInput()
-                #
-                #                  print("Time= " + time.strftime("%Y/%m/%d, %H:%M:%S", time.localtime()))
-                #                  print("dataCount= " + str(round(((dataCount * sampleEveryMicroSecond) / 1000), 6)) + "ms")
-                #                  dataCount = 0
-                #                  print(str(tatalmAmS) + " mA/mS")
-                #                  print(str(totalmAH) + " mAH")
-                #                  print("Write to File X")
-                #                  print("")
-                #             f.close()
-                # count = 0
-                # voltageSum = 0
-
-
 
 except KeyboardInterrupt:
     ser.close()  # 清除序列通訊物件
